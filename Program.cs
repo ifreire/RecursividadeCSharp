bbc0a6b3ecdbf3c5d29471a10c10d81c0a54ae2f
Iuri Freire <iuricostafreire@gmail.com>
2021-10-18 16:11:51 -0300

    1st commit.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RecursividadeCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recursividade(0);
+        }
+
+        static void Recursividade(int x)
+        {
+            if (x <= 100)
+            {
+                Console.WriteLine(x);
+                Recursividade(++x);
+            }
+
+            return;
+        }
+    }
+}
